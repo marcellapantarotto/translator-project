6bcf629dc6164f97a404ec8cdc1c3a7f2c9ff47f
Marcella Pantarotto <marcellapantarotto@gmail.com>
2021-09-07 12:06:27 -0300

    New tests

--- syntax-analyzer/tests/test_correct1.c
+++ syntax-analyzer/tests/test_correct1.c
@@ -1,11 +1,40 @@
+float list my_list;
+int list my_other_list;
+
 int main()
 {
-   int a;
-   int b;
-   float c;
+  int i;
+  int x;
+  my_list = NIL;
+  for (i = 0; i < 10; i = i + 1) {
+		writeln("Write 10 elements to the my_list: ");
+		read(x);
+		my_list = x : my_list;
+	}
 
-   if(a > b) {
-      read(c);
+   my_other_list >> my_list;
+
+   for (i = 0; i < 10; i = i + 1) {
+    x = 2 + i;
+    my_other_list = x : my_other_list;
+	}
+
+   if(my_list != NIL && my_list != NIL) {
+      writeln("Lists are not empty!");
    }
-   writeln(c);
+   
+   if (my_list > my_list ) {
+      writeln("My list is bigger");
+   } else if (my_list < my_list ) {
+      writeln("My list is smaller");
+   } else {
+      writeln("Both lists are the same size");
+   }
+
+   my_other_list << my_list;
+
+   writeln(! my_list);
+   writeln(% my_other_list);
+
+   return 0;
 }
--- syntax-analyzer/tests/test_correct2.c
+++ syntax-analyzer/tests/test_correct2.c
@@ -1,12 +1,41 @@
-int main() {
-   // Comment
-   float list my_list;
-   int list other_list;
+int main()
+{
+   int i;
+   int x;
+   x = 0;
+   int y;
+   y = 0;
+   int count;
+   count = 20;
+   int aux;
+   aux = 3;
 
-   if(my_list == other_list) {
-      write("Lists are the same");
-   } 
-   if(my_list != other_list) {
-      writeln("Lists are not the same");
+   for (i = 0; i < count; i = i+1)
+   {
+      x = x + i;
+      y = aux + y;
+      aux = aux +1;
    }
+
+   if (x < y && aux < 0 )
+   {
+      writeln("New day");
+   }
+
+   if (x > y || 7 > 5 )
+   {
+      writeln("Another day");
+   }
+
+   if (x < y && aux < 0 || x > y && 7 > 5 )
+   {
+      writeln("The sun is shinning");
+   }  
+
+   if (5 > 2 && 3 < 4 || x < y && 7 > 5 )
+   {
+      writeln("The sun is hot");
+   }  
+  
+  return 0;
 }
